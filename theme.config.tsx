73890b6319a86c102fb7a5b5195b6c4445ec8c01
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2023-04-02 21:55:39 +0200

    feat(theme): 👨🏽‍💻

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,6 +9,21 @@ export default {
             WebCie {new Date().getFullYear()}
         </span>,
     },
+    docsRepositoryBase: 'https://github.com/Fellenoord/wiki',
+    faviconGlyph: '👨🏽‍💻',
+    head: (
+      <>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta property="og:title" content="WebCieWiki" />
+          <meta property="og:description" content="The WebCieWiki." />
+      </>
+    ),
+    banner: {
+        key: 'join-webcie',
+        text: <a href="https://fellenoord.nl" target="_blank">
+            😉 Don't like the website? Join the WebCie →
+        </a>,
+    },
     useNextSeoProps() {
         return {
             titleTemplate: '%s'
