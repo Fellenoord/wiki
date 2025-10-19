b79ba383879bcfa04bb88443b5d14361bcdb0038
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2025-10-19 16:09:52 +0200

    chore: Rename WebCie to Webcie
    Webcie is the correct spelling.

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,12 +1,12 @@
 export default {
-    logo: <span style={{ fontWeight: 800 }}>WebCieWiki</span>,
+    logo: <span style={{ fontWeight: 800 }}>WebcieWiki</span>,
     project: {
         link: 'https://github.com/fellenoord',
     },
     darkMode: true,
     footer: {
         text: <span>
-            © {new Date().getFullYear()} WebCie Fellenoord
+            © {new Date().getFullYear()} Webcie Fellenoord
         </span>,
     },
     docsRepositoryBase: 'https://github.com/Fellenoord/wiki',
@@ -14,14 +14,14 @@ export default {
     head: (
       <>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta property="og:title" content="WebCieWiki" />
-          <meta property="og:description" content="The WebCieWiki." />
+          <meta property="og:title" content="WebcieWiki" />
+          <meta property="og:description" content="The WebcieWiki." />
       </>
     ),
     banner: {
         key: 'join-webcie',
         text: <a href="https://fellenoord.nl" target="_blank">
-            😉 Don't like the website? Join the WebCie →
+            😉 Don't like the website? Join the Webcie →
         </a>,
     },
     useNextSeoProps() {
